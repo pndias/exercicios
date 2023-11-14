9e7c5ee6ef7af38a3c8ed862df58b4dfdb2134a1
Pablo Dias <pablonns@gmail.com>
2023-11-14 14:46:43 -0300

    Terminado o exercício 1 da segunda lista

--- lista2/ex1.js
+++ lista2/ex1.js
@@ -14,4 +14,28 @@ function arrayPrint() {
   vetor[6] = 32;
   vetor[8] = 33;
   vetor[10] = 87;
+
+  // e) A posição 5 deve guardar 6 + 3 * o valor da sétima posição(6) a posição v[7] = v[5] + 10
+  vetor[5] = 5 + 3 * vetor[6];
+  vetor[7] = vetor[5] + 10;
+
+  // f) O valor 72 deve ser inserido na posição dada pelo valor da quinta posição
+  vetor[vetor[4]] = 72;
+
+  // g) A posição 4 recebe o valor da oitava posição
+  vetor[4] = vetor[7];
+  // h) troque o valor da 11a posição com o da 6a
+  let temp = vetor[5];
+  vetor[5] = vetor[10];
+  vetor[10] = temp;
+  // i) Troque os valores das posições 0 e 8
+  temp = vetor[0];
+  vetor[0] = vetor[8];
+  vetor[8] = temp;
+  // j) escrever todas as posições de trás pra frente usando casting para char
+  for (let i = vetor.length - 1; i >= 0; i--) {
+    console.log(String.fromCharCode(vetor[i]));
+  }
 }
+
+arrayPrint();
